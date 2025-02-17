ec12c14b21f67b050b01b0b33094cd8c65c1ec4a
Rajeshkumar-G-FSD <therkpotter@gmail.com>
2025-02-17 22:37:01 +0530

    added DB

--- app.js
+++ app.js
@@ -12,12 +12,16 @@ app.use(bodyParser.json());
 app.use('/api', recipeRoutes);
 const URL = process.env.DB || "mongodb://localhost:27017";
 mongoose.connect(URL);
+console.log(URL);
 
 // MongoDB Connection
 mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 100000, // 30 seconds
+    tls: true,
+  tlsAllowInvalidCertificates: false, // Set to true only for testing with self-signed certificates
+  loggerLevel: 'debug',
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error(err));
